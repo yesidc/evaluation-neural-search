268c8242b04bb4abca93f7f9e2da74581b93cdc0
Yesid Cano <yesidcanoc@gmail.com>
2023-06-22 21:56:34 +0200

    Quantized Model

--- encoding/encode_image_query.py
+++ encoding/encode_image_query.py
@@ -10,9 +10,9 @@ import torch
 # todo this model should not always be loaded (load asynchronously), but only when the user uploads media.
 # todo to consider: barch inference instead of encoding an image at a time.
 model = SentenceTransformer('clip-ViT-B-32')
-# model = quantize_dynamic(model, {nn.Linear}, dtype=torch.qint8)
+model = quantize_dynamic(model, {nn.Linear}, dtype=torch.qint8)
 text_model = SentenceTransformer('sentence-transformers/clip-ViT-B-32-multilingual-v1')
-# text_model = quantize_dynamic(text_model, {nn.Linear}, dtype=torch.qint8)
+text_model = quantize_dynamic(text_model, {nn.Linear}, dtype=torch.qint8)
 
 
 def encode_image(file):
