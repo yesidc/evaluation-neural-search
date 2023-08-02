db5cdcc9a5a54df77b231bc5e546be907190184a
Yesid Cano <yesidcanoc@gmail.com>
2023-08-02 22:50:21 +0200

    computed (non-quantized) model size

--- utils/model_size.py
+++ utils/model_size.py
@@ -3,6 +3,11 @@ from torch.quantization import quantize_dynamic
 import torch.nn as nn
 import torch
 from pathlib import Path
+import logging
+
+from logger_evaluation import logger
+
+#file_logger = logging.getLogger('evaluation')
 
 
 image_model = SentenceTransformer('clip-ViT-B-32')
@@ -13,7 +18,7 @@ text_model = SentenceTransformer('sentence-transformers/clip-ViT-B-32-multilingu
 
 
 # uncomment to quantize the model
-#text_model = quantize_dynamic(text_model, {nn.Linear}, dtype=torch.qint8)
+# text_model = quantize_dynamic(text_model, {nn.Linear}, dtype=torch.qint8)
 
 state_dict_img = image_model.state_dict()
 state_dict_text = text_model.state_dict()
@@ -34,14 +39,12 @@ size_text = Path(tmp_path_text).stat().st_size / (1024 * 1024)
 tmp_path_img.unlink()
 tmp_path_text.unlink()
 
-print(f'Image model size: {size_img:.2f} MB \n')
-print(f'Text model size: {size_text:.2f} MB')
+logger.info(f'Image model size (Non-quantized): {size_img:.2f} MB')
+logger.info(f'Text model size (Non-quantized): {size_text:.2f} MB')
 
 
 """
 Size of the models:
 Image model size (Non-quantized): 577.2260179519653 MB --> 577.23 MB
-Text model size (Non-quantized): 515.5073661804199 MB --> 515.51 MB
-
-
+Text model size (Non-quantized): 515.5073661804199 MB -->  515.51 MB
 """
